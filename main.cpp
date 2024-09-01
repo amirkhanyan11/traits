d179959235026379fe7842ba69c804a2e8d30799
amirkhanyan11 <artyom.amirkhanyan11@gmail.com>
2024-09-01 20:43:50 +0400

    chang

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include "traits.hpp"
 #include <iostream>
 
+using std::cout, std::endl;
+
+constexpr bool p(int x)
+{
+	return x >= 0;
+}
 
 int main()
 {
-	
+
+	cocobolo::conditional_t<p(-9), int, double> c = 5.3;
+
+	cout << typeid(c).name() << endl;
+
+	return 0;
 }
--- traits.hpp
+++ traits.hpp
@@ -3,6 +3,182 @@
 #define __TRAITS___
 
 
+#include <type_traits>
+
+
+namespace cocobolo
+{
+
+using std::false_type, std::true_type;
+
+
+template <class T, class U>
+struct is_same : false_type {};
+
+template <class T>
+struct is_same<T, T> : true_type {};
+
+template <typename T, typename U, typename... Ts>
+struct is_all_same
+{
+	static constexpr bool value = is_same<T, U>::value || is_all_same<U, Ts...>::value;
+};
+
+template <typename T, typename U>
+struct is_all_same<T, U>
+{
+	static constexpr bool value = is_same<T, U>::value;
+};
+
+
+
+
+
+template <typename T>
+struct remove_const
+{
+	using type = T;
+};
+
+template <typename T>
+struct remove_const<const T>
+{
+	using type = T;
+};
+
+template <typename T>
+using remove_const_t = typename remove_const<T>::type;
+
+template <typename T>
+struct remove_volatile
+{
+	using type = T;
+};
+
+template <typename T>
+struct remove_volatile<volatile T>
+{
+	using type = T;
+};
+
+template <typename T>
+using remove_volatile_t = typename remove_volatile<T>::type;
+
+template <typename T>
+struct remove_cv
+{
+	using type = typename remove_const<typename remove_volatile<T>::type>::type;
+};
+
+template <typename T>
+using remove_cv_t = typename remove_cv<T>::type;
+
+
+
+
+
+template <typename T>
+struct add_const
+{
+	using type = const T;
+};
+
+template <typename T>
+using add_const_t = typename add_const<T>::type;
+
+
+template <typename T>
+struct add_volatile
+{
+	using type = volatile T;
+};
+
+template <typename T>
+using add_volatile_t = typename add_volatile<T>::type;
+
+template <typename T>
+struct add_cv
+{
+	using type = typename add_const<typename add_volatile<T>::type>::type;
+};
+
+template <typename T>
+using add_cv_t = typename add_cv<T>::type;
+
+
+
+
+
+template <typename T>
+struct is_reference : false_type {};
+
+template <typename T>
+struct is_reference<T&> : true_type {};
+
+template <typename T>
+struct is_reference<T&&> : true_type {};
+
+template <typename T>
+struct remove_reference
+{
+	using type = T;
+};
+
+template <typename T>
+struct remove_reference<T&>
+{
+	using type = T;
+};
+
+template <typename T>
+struct remove_reference<T&&>
+{
+	using type = T;
+};
+
+template <typename T>
+using remove_reference_t = typename remove_reference<T>::type;
+
+
+template <typename T>
+struct add_lvalue_reference
+{
+	using type = T&;
+};
+
+template <typename T>
+using add_lvalue_reference_t = typename add_lvalue_reference<T>::type;
+
+template <typename T>
+struct add_rvalue_reference
+{
+	using type = T&&;
+};
+
+template <typename T>
+using add_rvalue_reference_t = typename add_rvalue_reference<T>::type;
+
+
+
+
+
+
+template <bool val, typename T, typename U>
+struct conditional
+{
+	using type = T;
+};
+
+
+template <typename T, typename U>
+struct conditional<false, T, U>
+{
+	using type = U;
+};
+
+
+template <bool val, typename T, typename U>
+using conditional_t = typename conditional<val, T, U>::type;
 
 
 
@@ -10,7 +186,7 @@
 
 
 
-
+};
 
 
 
